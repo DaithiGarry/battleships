d5c33e85d7cb290bfdf75a37aed3a7d90a9946eb
David Garry <daithigarry@hotmail.com>
2022-06-29 12:23:50 +0000

    A simple Class board has been defined

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class Board:
+    #define user board
+    def _init_(self, board):
+        self.board = board
+    
+    #define the range of letters and numbers on the user board
+    def get_letters_to_numbers():
+        letters_to_numbers = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7, "I":8}
+        return letters_to_numbers
+
+    #define the appearance of the userboard via printing labels for ABCD etc and create a spacer beneath -.
+    def print_board(self):
+        print("A B C D E F G H I")
+        print("- - - - - - - - -")
+        row_number = 1
+        #create a loop to loop through the board the correct numbers of times(%d|%S|) and print the row numbers(which are performed in increments of 1) for each loop which has been spaced by a |.
+        for row in self.board:
+            print("%d|%S|" % (row_number, "|".join(row)))
+            row_number +=1
