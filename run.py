bc02df1d3770e465c6b538f8097b1aef2054bd11
David Garry <daithigarry@hotmail.com>
2022-06-30 13:13:15 +0000

    class battleship has been redefined to simply the get_ships function which is now removed. A read interger function has been added in order to allow user to enter a value between min and max for changing the board size

--- run.py
+++ run.py
@@ -1,5 +1,29 @@
 from random import randint
 
+Guesses_Count = 5
+Board_Size_Row = 8
+Board_Size_Column = 8
+
+Hidden = "-"
+Ship = "X"
+Guess = "0"
+
+def read_int(prompt: str, min_value: int = 1, max_value: int = 8) -> int:
+    """Read an integer between a min and max value."""
+
+    while True:
+        line = input(prompt)
+        try:
+            value = int(line)
+            if value < min_value:
+                print(f"The minimum value is {min_value}. Try again.")
+            elif value > max_value:
+                print(f"The maximum value is {max_value}. Try again.")
+            else:
+                return value
+        except ValueError:
+            print("That's not a number! Try again.")
+
 class GameBoard:
     """
     define user board
@@ -22,34 +46,33 @@ class GameBoard:
         print("A B C D E F G H I")
         print("- - - - - - - - -")
         row_number = 1
-        """
-        create a loop to loop through the board the correct numbers of times(%d|%S|) and print the row numbers(which are performed in increments of 1) for each loop which has been spaced by a |.
-        """
+       
         for row in self.board:
+            """
+            define a loop to run through the board the correct numbers
+            """
             print("%d|%s|" % (row_number, "|".join(row)))
             row_number += 1
 
 class Battleship:
     """ 
-    the same function was used as was created in class Board above.
+    The battleship class is used to define the battleship board and the functions to identify ships, hits and misses.
     """
-    def _init_(self, board):
-        self.board = board
+    def _init_(self, size_x: int, size_y: int) -> None:
+        #creating the board/grid function
+        self.board = [[Hidden] * size_x for _ in range (size_y)]
+        #Create ships function has been removed and added here
+        self.x_row = random.randint(0, size_x -1)
+        self.y_column = random.randint(0, size_y -1)
+        self.board[self.x_row][self.y_column] = Ship
 
-    def create_ships(self):
-        """ 
-        # run a loop to find a random place to put an X, the "while" parameter will make sure that no existing battleship has been placed in this location and loop again if it is occupied
-        """
-        for i in range(5):
-            self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
-            while self.board[self.x_row][self.y_column] == "X":
-                self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
-            self.board[self.x_row][self.y_column] = "X"
-        return self.board
+    
     
     def get_player_input(self):
         """ 
-        player input functions are created to make sure the player selects an existing row (x) and column (y) to insure the instructions to the program are valid.
+        player input functions are created to make sure the player selects 
+        an existing row (x) and column (y) to insure the 
+        instructions to the program are valid.
         """
         try:
             x_row = input("Enter the row of the ship: e.g. 123 ")
@@ -61,14 +84,15 @@ class Battleship:
             while y_column not in "ABCDEFGHI":
                 print("Cannot place ship here, please select a column")
                 y_column = input("Enter the column of the ship: e.g. ABC ")
-            return int(x_row) -1, Board.get_letters_to_numbers()[y_column]
+            return int(x_row) -1, GameBoard.get_letters_to_numbers()[y_column]
         except ValueError() and KeyError():
             print("Input is not valid")
             return self.get_player_input()
     
     def hit_ships_counter(self):
         """ 
-        hit ships counter to start at 0, and will increase by 1 for each X (direct hit) that is found on the board.
+        hit ships counter to start at 0, and will increase by 1 for each 
+        X (direct hit) that is found on the board.
         """
         hit_ships = 0
         for row in self.board:
@@ -79,16 +103,17 @@ class Battleship:
 
     def RunGame():
         """ 
-        defining the board ranges for both the computer and the player boards. Loop for player attempts and insure locations cannot be selected twice.
+        defining the board ranges for both the computer and the player boards. 
+        Loop for player attempts and insure locations cannot be selected twice.
         """
-        computer_board = Board([[" "] * 8 for i in range(8)])
-        player_board = Board([[" "] * 8 for i in range(8)])
+        computer_board = GameBoard([[" "] * 8 for i in range(8)])
+        player_board = GameBoard([[" "] * 8 for i in range(8)])
         Battleship.create_ships(computer_board)
         attempts = 10
         while attempts > 0:
-            Board.print_board(player_board)
+            GameBoard.print_board(player_board)
             player_x_row, player_y_column = Battleship.get_player_input(object)
-        while player_board.board[player_x_row][player_y_column] == "0" or player_board.board[player_x_row][player_y_column] == "X":
+        while player_board.board[player_x_row][player_y_column] == "0"or player_board.board[player_x_row][player_y_column] == "X":
             print("Cannot complete, you have already guessed this location")
             player_x_row, player_y_column = Battleship.get_player_input(object)
         if computer_board.board[player_x_row][player_y_column] == "X":
@@ -104,7 +129,7 @@ class Battleship:
             print(f"You have {attempts} attempts left!")
             if attempts == 0:
                 print("Sorry, no attempts remaining")
-                Board.print_board(player_board)
+                GameBoard.print_board(player_board)
                 
 print("Welcome to Battleships!")
 if __name__ == "__main__":
