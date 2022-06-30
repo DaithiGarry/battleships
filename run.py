78c4b73c31ffa8d1065984b7c61cf9af79f78dd0
David Garry <daithigarry@hotmail.com>
2022-06-30 10:58:31 +0000

    comments have been replaced with docstrings

--- run.py
+++ run.py
@@ -1,36 +1,47 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 import random
 
 class Board:
-    #define user board
+    """
+    define user board
+    """
     def _init_(self, board):
         self.board = board
-    
-    #define the range of letters and numbers on the user board
+
     def get_letters_to_numbers():
+        """
+        define the range of letters and numbers on the user board
+        """
         letters_to_numbers = {"A":0, "B":1, "C":2, "D":3, "E":4, "F":5, "G":6, "H":7, "I":8}
         return letters_to_numbers
 
-    #define the appearance of the userboard via printing labels for ABCD etc and create a spacer beneath -.
+    
     def print_board(self):
+        """
+        define the appearance of the userboard via printing labels for ABCD etc and create a spacer beneath -.
+        """
         print("A B C D E F G H I")
         print("- - - - - - - - -")
         row_number = 1
-        #create a loop to loop through the board the correct numbers of times(%d|%S|) and print the row numbers(which are performed in increments of 1) for each loop which has been spaced by a |.
+        """
+        create a loop to loop through the board the correct numbers of times(%d|%S|) and print the row numbers(which are performed in increments of 1) for each loop which has been spaced by a |.
+        """
         for row in self.board:
             print("%d|%S|" % (row_number, "|".join(row)))
             row_number +=1
 
 class Battleship:
-    # the same function was used as was created in class Board above.
+    """ 
+    the same function was used as was created in class Board above.
+    """
     def _init_(self, board):
         self.board = board
 
     def create_ships(self):
+        """ 
+        # run a loop to find a random place to put an X, the "while" parameter will make sure that no existing battleship has been placed in this location and loop again if it is occupied
+        """
         for i in range(5):
-            # run a loop to find a random place to put an X, the "while" parameter will make sure that no existing battleship has been placed in this location and loop again if it is occupied
             self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
             while self.board[self.x_row][self.y_column] == "X":
                 self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
@@ -38,7 +49,9 @@ class Battleship:
         return self.board
     
     def get_player_input(self):
-        # player input functions are created to make sure the player selects an existing row (x) and column (y) to insure the instructions to the program are valid.
+        """ 
+        player input functions are created to make sure the player selects an existing row (x) and column (y) to insure the instructions to the program are valid.
+        """
         try:
             x_row = input("Enter the row of the ship: e.g. 123 ")
             while x_row not in "12345678":
@@ -55,7 +68,9 @@ class Battleship:
             return self.get_player_input()
     
     def hit_ships_counter(self):
-        #hit ships counter to start at 0, and will increase by 1 for each X (direct hit) that is found on the board.
+        """ 
+        hit ships counter to start at 0, and will increase by 1 for each X (direct hit) that is found on the board.
+        """
         hit_ships = 0
         for row in self.board:
             for column in row:
@@ -64,6 +79,9 @@ class Battleship:
         return hit_ships
 
     def RunGame():
+        """ 
+        defining the board ranges for both the computer and the player boards. Loop for player attempts and insure locations cannot be selected twice.
+        """
         computer_board = Board([[" "] * 8 for i in range(8)])
         player_board = Board([[" "] * 8 for i in range(8)])
         Battleship.create_ships(computer_board)
@@ -89,6 +107,6 @@ class Battleship:
                 print("Sorry, no attempts remaining")
                 Board.print_board(player_board)
                 
-print("Welcome to Battleships")
+print("Welcome to Battleships!")
 if __name__ == "__main__":
     RunGame()
